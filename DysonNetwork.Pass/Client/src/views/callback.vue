3df66dabd9ff43ff660345b278ac66edcecec3b1
LittleSheep <littlesheep.code@hotmail.com>
2025-08-03 12:39:10 +0800

    :bug: Fix callback is not centered

--- DysonNetwork.Pass/Client/src/views/callback.vue
+++ DysonNetwork.Pass/Client/src/views/callback.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex justify-center items-center">
+  <div class="h-full flex items-center justify-center">
     <n-result status="success" title="Auth completed" description="Now you can close this tab" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { NResult } from 'naive-ui';
+import { NResult } from 'naive-ui'
 </script>
