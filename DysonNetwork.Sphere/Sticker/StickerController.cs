f1ea7c1c5ac59c0f274ea6d21614590a4eb79742
LittleSheep <littlesheep.code@hotmail.com>
2025-08-07 13:14:45 +0800

    :bug: Fix sticker open on gateway

--- DysonNetwork.Sphere/Sticker/StickerController.cs
+++ DysonNetwork.Sphere/Sticker/StickerController.cs
@@ -215,7 +215,7 @@ public class StickerController(AppDatabase db, StickerService st, FileService.Fi
         var sticker = await st.LookupStickerByIdentifierAsync(identifier);
 
         if (sticker?.Image is null) return NotFound();
-        return Redirect($"/cgi/drive/files/{sticker.Image.Id}");
+        return Redirect($"/drive/files/{sticker.Image.Id}");
     }
 
     [HttpGet("{packId:guid}/content/{id:guid}")]
