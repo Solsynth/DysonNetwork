636b674229c91f21b50026795bfed0443a8c73cf
LittleSheep <littlesheep.code@hotmail.com>
2025-08-19 19:23:41 +0800

    :bricks: Add stream (NATS) message queue infra

---
+++ DysonNetwork.Shared/Stream/AccountEvent.cs
@@ -0,0 +1,9 @@
+using NodaTime;
+
+namespace DysonNetwork.Shared.Stream;
+
+public class AccountDeletedEvent
+{
+    public Guid AccountId { get; set; } = Guid.NewGuid();
+    public Instant DeletedAt { get; set; } = SystemClock.Instance.GetCurrentInstant();
+}
