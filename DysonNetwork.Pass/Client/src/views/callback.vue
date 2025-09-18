1e374a73c743da2795703b1cbe32cdd50a6db3d4
LittleSheep <littlesheep.code@hotmail.com>
2025-09-19 00:11:26 +0800

    :wastebasket: Remove the built-in frontends

--- DysonNetwork.Pass/Client/src/views/callback.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="h-full flex items-center justify-center">
-    <n-result status="success" title="Auth completed" description="Now you can close this tab" />
-  </div>
-</template>
-
-<script lang="ts" setup>
-import { NResult } from 'naive-ui'
-</script>
