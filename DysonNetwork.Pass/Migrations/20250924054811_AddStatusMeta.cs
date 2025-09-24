ff9d49086976c4288dee537978ace63cc45e418b
LittleSheep <littlesheep.code@hotmail.com>
2025-09-24 13:48:36 +0800

    :card_file_box: Update status migration

---
+++ DysonNetwork.Pass/Migrations/20250924054811_AddStatusMeta.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Pass.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusMeta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Dictionary<string, object>>(
+                name: "meta",
+                table: "account_statuses",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "meta",
+                table: "account_statuses");
+        }
+    }
+}
