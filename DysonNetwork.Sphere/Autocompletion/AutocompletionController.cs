15687a0c328d427d90c3cff25bb17dfba4ab6665
LittleSheep <littlesheep.code@hotmail.com>
2025-10-12 16:59:26 +0800

    :sparkles: Standalone auto complete

---
+++ DysonNetwork.Sphere/Autocompletion/AutocompletionController.cs
@@ -0,0 +1,20 @@
+using DysonNetwork.Shared.Models;
+using DysonNetwork.Shared.Proto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DysonNetwork.Sphere.Autocompletion;
+
+[ApiController]
+[Route("/api/autocomplete")]
+public class AutocompletionController(AutocompletionService aus) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<List<DysonNetwork.Shared.Models.Autocompletion>>> TextAutocomplete([FromBody] AutocompletionRequest request, Guid roomId)
+    {
+        if (HttpContext.Items["CurrentUser"] is not Account currentUser)
+            return Unauthorized();
+
+        var result = await aus.GetAutocompletion(request.Content, chatId: roomId, limit: 10);
+        return Ok(result);
+    }
+}
