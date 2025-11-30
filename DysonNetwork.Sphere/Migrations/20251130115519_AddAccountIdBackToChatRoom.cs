e97719ec84c0c9e2eceb5b50f6f09eeedf4f7094
LittleSheep <littlesheep.code@hotmail.com>
2025-11-30 20:13:15 +0800

    :card_file_box: Add missing account id migrations

---
+++ DysonNetwork.Sphere/Migrations/20251130115519_AddAccountIdBackToChatRoom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Sphere.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountIdBackToChatRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "account_id",
+                table: "chat_rooms",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "account_id",
+                table: "chat_rooms");
+        }
+    }
+}
