1af11b2a99ada09b5b1337be7cb7451d35611c5d
LittleSheep <littlesheep.code@hotmail.com>
2025-12-25 22:47:01 +0800

    :sparkles: Account rewind controller

--- DysonNetwork.Pass/Rewind/AccountRewindController.cs
+++ DysonNetwork.Pass/Rewind/AccountRewindController.cs
@@ -1,6 +1,19 @@
+using DysonNetwork.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DysonNetwork.Pass.Rewind;
 
-public class AccountRewindController
+[ApiController]
+[Route("/api/rewind")]
+public class AccountRewindController(AccountRewindService rewindSrv) : ControllerBase
 {
-    
+    [HttpGet("me")]
+    [Authorize]
+    public async Task<ActionResult<SnRewindPoint>> GetCurrentRewindPoint()
+    {
+        if (HttpContext.Items["CurrentUser"] is not SnAccount currentUser) return Unauthorized();
+        var point = await rewindSrv.GetOrCreateRewindPoint(currentUser.Id);
+        return Ok(point);
+    }
 }
