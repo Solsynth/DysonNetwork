334fa9b9a7dd3a2509ac4ecd777a29e2e89f1372
LittleSheep <littlesheep.code@hotmail.com>
2025-12-27 14:25:08 +0800

    :card_file_box: Sharable rewind point migration

---
+++ DysonNetwork.Pass/Migrations/20251227062459_AddSharableRewindPoint.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Pass.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSharableRewindPoint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "sharable_code",
+                table: "rewind_points",
+                type: "character varying(4096)",
+                maxLength: 4096,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sharable_code",
+                table: "rewind_points");
+        }
+    }
+}
