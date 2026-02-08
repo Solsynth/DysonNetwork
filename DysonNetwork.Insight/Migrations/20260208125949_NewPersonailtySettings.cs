f893ab1caa757c2bdfcf6a2d43a523e92cdc9aa6
LittleSheep <littlesheep.code@hotmail.com>
2026-02-08 21:00:29 +0800

    :card_file_box: Migrate insight changes

---
+++ DysonNetwork.Insight/Migrations/20260208125949_NewPersonailtySettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Insight.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPersonailtySettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
