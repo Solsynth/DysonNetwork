762198da684006696460618dc02e89bdf1390aba
LittleSheep <littlesheep.code@hotmail.com>
2026-02-13 00:46:33 +0800

    :card_file_box: Improve the thought table

---
+++ DysonNetwork.Insight/Migrations/20260212164620_CleanUpThoughtTable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using DysonNetwork.Shared.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Insight.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanUpThoughtTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "files",
+                table: "thinking_thoughts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<SnCloudFileReferenceObject>>(
+                name: "files",
+                table: "thinking_thoughts",
+                type: "jsonb",
+                nullable: false);
+        }
+    }
+}
