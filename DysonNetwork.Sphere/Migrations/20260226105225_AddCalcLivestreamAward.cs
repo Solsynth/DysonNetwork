9e2901b40c1701300c080bd3c4cd07ddf63a65ea
LittleSheep <littlesheep.code@hotmail.com>
2026-02-26 18:53:30 +0800

    :card_file_box: Update sphere db

---
+++ DysonNetwork.Sphere/Migrations/20260226105225_AddCalcLivestreamAward.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Sphere.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCalcLivestreamAward : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "distributed_award_amount",
+                table: "live_streams",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "distributed_award_amount",
+                table: "live_streams");
+        }
+    }
+}
