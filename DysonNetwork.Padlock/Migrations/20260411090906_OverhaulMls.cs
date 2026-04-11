06c61b203852a98583b6ba032443f86881fed55b
LittleSheep <hi@littlesheep.me>
2026-04-11 17:09:30 +0800

    :card_file_box: Update MLS db

---
+++ DysonNetwork.Padlock/Migrations/20260411090906_OverhaulMls.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Padlock.Migrations
+{
+    /// <inheritdoc />
+    public partial class OverhaulMls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "group_info",
+                table: "mls_group_states",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ratchet_tree",
+                table: "mls_group_states",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "group_info",
+                table: "mls_group_states");
+
+            migrationBuilder.DropColumn(
+                name: "ratchet_tree",
+                table: "mls_group_states");
+        }
+    }
+}
