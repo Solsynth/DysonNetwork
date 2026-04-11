8f40d9ed39ade9ae5eb045285a890d469f652da0
LittleSheep <hi@littlesheep.me>
2026-04-11 14:10:31 +0800

    :card_file_box: Update drive migration

---
+++ DysonNetwork.Drive/Migrations/20260411060935_AddDeletedAtToUniqueIndexes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Drive.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeletedAtToUniqueIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_bundles_slug",
+                table: "bundles");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bundles_slug_deleted_at",
+                table: "bundles",
+                columns: new[] { "slug", "deleted_at" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_bundles_slug_deleted_at",
+                table: "bundles");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bundles_slug",
+                table: "bundles",
+                column: "slug",
+                unique: true);
+        }
+    }
+}
