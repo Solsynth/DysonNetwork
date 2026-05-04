24ed8ac56b13386e75a541faba57c77937e9008b
LittleSheep <hi@littlesheep.me>
2026-05-04 16:47:40 +0800

    :card_file_box: Add multiwallet migration

---
+++ DysonNetwork.Wallet/Migrations/20260504084724_AddMultiWallet.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Wallet.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMultiWallet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_wallets_public_id",
+                table: "wallets");
+
+            migrationBuilder.DropIndex(
+                name: "ix_wallets_realm_id",
+                table: "wallets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_wallets_public_id",
+                table: "wallets",
+                column: "public_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_wallets_realm_id",
+                table: "wallets",
+                column: "realm_id");
+        }
+    }
+}
