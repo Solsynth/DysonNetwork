9310592b478bf2476b69337a2570e6a2dedb7014
LittleSheep <hi@littlesheep.me>
2026-05-11 23:50:48 +0800

    :card_file_box: Post index migration

--- DysonNetwork.Sphere/Migrations/20260511155026_AddPostEmbeddingIndex.Designer.cs
+++ DysonNetwork.Sphere/Migrations/20260511155026_AddPostEmbeddingIndex.Designer.cs
@@ -6,6 +6,7 @@ using DysonNetwork.Shared.Models;
 using DysonNetwork.Sphere;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NodaTime;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -16,9 +17,11 @@ using Pgvector;
 namespace DysonNetwork.Sphere.Migrations
 {
     [DbContext(typeof(AppDatabase))]
-    partial class AppDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20260511155026_AddPostEmbeddingIndex")]
+    partial class AddPostEmbeddingIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
