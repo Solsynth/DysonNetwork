d22c5405400d7a6903c1f14a55383708e27d4833
LittleSheep <hi@littlesheep.me>
2026-05-21 01:34:05 +0800

    :card_file_box: Ring's migration

---
+++ DysonNetwork.Ring/Migrations/20260520173347_AddDeviceNameToSubscription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Ring.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceNameToSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "device_name",
+                table: "push_subscriptions",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "device_name",
+                table: "push_subscriptions");
+        }
+    }
+}
