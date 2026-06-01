675a9fdeca6dbc95cf27854c25062497ceda48fa
LittleSheep <hi@littlesheep.me>
2026-06-02 01:12:15 +0800

    :card_file_box: Event calendar model changes

---
+++ DysonNetwork.Passport/Migrations/20260601171159_EnrichCalendarEvent.cs
@@ -0,0 +1,39 @@
+﻿using DysonNetwork.Shared.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Passport.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnrichCalendarEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<SnCloudFileReferenceObject>(
+                name: "background",
+                table: "account_statuses",
+                type: "jsonb",
+                nullable: true);
+
+            migrationBuilder.AddColumn<SnCloudFileReferenceObject>(
+                name: "icon",
+                table: "account_statuses",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "background",
+                table: "account_statuses");
+
+            migrationBuilder.DropColumn(
+                name: "icon",
+                table: "account_statuses");
+        }
+    }
+}
