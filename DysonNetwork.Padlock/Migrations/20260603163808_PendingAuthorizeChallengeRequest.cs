2e70644fd65d512e690a37ef920dc46b26b0aea6
LittleSheep <hi@littlesheep.me>
2026-06-04 00:38:23 +0800

    :card_file_box: Pending challenge authorize request

---
+++ DysonNetwork.Padlock/Migrations/20260603163808_PendingAuthorizeChallengeRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace DysonNetwork.Padlock.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingAuthorizeChallengeRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Instant>(
+                name: "approved_at",
+                table: "auth_challenges",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "approved_by_session_id",
+                table: "auth_challenges",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Instant>(
+                name: "declined_at",
+                table: "auth_challenges",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "approved_at",
+                table: "auth_challenges");
+
+            migrationBuilder.DropColumn(
+                name: "approved_by_session_id",
+                table: "auth_challenges");
+
+            migrationBuilder.DropColumn(
+                name: "declined_at",
+                table: "auth_challenges");
+        }
+    }
+}
