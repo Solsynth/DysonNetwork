62d7e4fe77b8d6550feb849cac087a5810b38446
LittleSheep <hi@littlesheep.me>
2026-07-09 14:59:42 +0800

    feat: add migration for IsPublic on AccountConnection

---
+++ DysonNetwork.Padlock/Migrations/20260709065927_AddIsPublicToAccountConnection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DysonNetwork.Padlock.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPublicToAccountConnection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_public",
+                table: "account_connections",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_public",
+                table: "account_connections");
+        }
+    }
+}
